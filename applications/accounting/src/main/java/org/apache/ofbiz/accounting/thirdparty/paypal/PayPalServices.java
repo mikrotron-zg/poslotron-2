1d6d655a75056272211fd9bf0a8aaad3c53951d4
Jacques Le Roux <jacques.le.roux@les7arts.com>
2026-04-14 14:57:40 +0200

    Fixed: Possible NPE in PayPal services (OFBIZ-13385)
    In PayPalServices::setExpressCheckout, using cart before checking if it is null
    
    ShoppingCart cart = (ShoppingCart) context.get("cart");
    Locale locale = cart.getLocale();
    if (cart == null || cart.items().size() <= 0) {
        return ServiceUtil.returnError(UtilProperties.getMessage(RESOURCE,
                 "AccountingPayPalShoppingCartIsEmpty", locale));
    }
    
    Thanks: Vitaly Repetenko

--- applications/accounting/src/main/java/org/apache/ofbiz/accounting/thirdparty/paypal/PayPalServices.java
+++ applications/accounting/src/main/java/org/apache/ofbiz/accounting/thirdparty/paypal/PayPalServices.java
@@ -95,9 +95,11 @@ public class PayPalServices {
     public static Map<String, Object> setExpressCheckout(DispatchContext dctx, Map<String, ? extends Object> context) {
         ShoppingCart cart = (ShoppingCart) context.get("cart");
         Locale locale = cart.getLocale();
-        if (cart == null || cart.items().size() <= 0) {
-            return ServiceUtil.returnError(UtilProperties.getMessage(RESOURCE,
-                    "AccountingPayPalShoppingCartIsEmpty", locale));
+        if (cart != null ) {
+            if (cart.items().size() <= 0) {
+                return ServiceUtil.returnError(UtilProperties.getMessage(RESOURCE,
+                        "AccountingPayPalShoppingCartIsEmpty", locale));
+            }
         }
 
         GenericValue payPalConfig = getPaymentMethodGatewayPayPal(dctx, context, null);
